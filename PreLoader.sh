bacd029791b36d45449c437d78d427d37780797d
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-20 03:58:15 -0500

    Fix quoting in PreLoader.sh for source command

--- PreLoader.sh
+++ PreLoader.sh
@@ -3,8 +3,8 @@
 # PreLoader.sh
 # This script will load our config and functions.
 #
-if [ -f $(dirname "$0")/PreLoader.bfunc ]; then 
-	source $(dirname "$0")/PreLoader.bfunc
+if [ -f "$(dirname $0)"/PreLoader.bfunc ]; then 
+	source "$(dirname $0)"/PreLoader.bfunc
 fi
 
 PreLoader_Config
