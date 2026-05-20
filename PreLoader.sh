13f507ea2c2fd7a593f55a7f3d6b7ae2ffcbc52d
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-20 04:00:38 -0500

    Fix indentation and formatting in PreLoader.sh

--- PreLoader.sh
+++ PreLoader.sh
@@ -3,9 +3,10 @@
 # PreLoader.sh
 # This script will load our config and functions.
 #
-if [ -f "$(dirname $0)"/PreLoader.bfunc ]; then 
-	source "$(dirname $0)"/PreLoader.bfunc
+if [ -f "$(dirname $0)/PreLoader.bfunc" ]; then
+    source "$(dirname $0)/PreLoader.bfunc"
 fi
+
 
 PreLoader_Config
 PreLoader_Functions
